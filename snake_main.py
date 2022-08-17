56f9a76298ef0ece18d0490161030fade762bc53
YatoNoragami1 <maximus17345@gmail.com>
2022-08-17 11:29:10 +0200

    added controller for input

--- snake_main.py
+++ snake_main.py
@@ -11,9 +11,10 @@ window_y = 1000
 game_window = pygame.display.set_mode((window_x, window_y))
 
 clock = pygame.time.Clock()
-fps = 30
+fps = 60
 
 pygame.init() 
+pygame.joystick.init()
 pygame.display.set_caption('Snake')
 game_window = pygame.display.set_mode((window_x, window_y))
 
@@ -38,15 +39,25 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        direction = 'UP'
-    elif keys[pygame.K_DOWN]:
-        direction = 'DOWN'
-    elif keys[pygame.K_RIGHT]:
-        direction = 'RIGHT'
-    elif keys[pygame.K_LEFT]:
-        direction = 'LEFT'
+    joystick_count = pygame.joystick.get_count()
+    for i in range(joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+        axes = joystick.get_numaxes()
+        for i in range(axes):
+            axis = joystick.get_axis(i)
+            print(axis)
+            if i == 0:
+                if axis > 0.0:
+                    direction = 'RIGHT'
+                if axis < 0.0:
+                    direction = 'LEFT'
+            if i == 1:
+                if axis > 0.0:
+                    direction = 'DOWN'
+                if axis < 0.0:
+                    direction = 'UP'
+
     
     if direction == 'RIGHT':
         Snake_pos[0] += 10
@@ -56,8 +67,8 @@ while running:
         Snake_pos[1] -= 10
     elif direction == 'DOWN':
         Snake_pos[1] += 10
-    game_window.fill(BLACK)
 
+    game_window.fill(BLACK)
     Snake.insert(0, list(Snake_pos))
     if Snake_pos[0] == apple[0] and Snake_pos[1] == apple[1]:
         apple = [random.randrange(0, window_x+1, 10), random.randrange(0, window_y+1, 10)]
@@ -88,3 +99,18 @@ while running:
 
     score(apples_eaten)
     pygame.display.update()
+
+
+
+
+
+#amogus   
+if direction == 'RIGHT':
+    Snake_pos[0] += 10
+elif direction == 'LEFT':
+    Snake_pos[0] -= 10
+elif direction == 'UP':
+    Snake_pos[1] -= 10
+elif direction == 'DOWN':
+    Snake_pos[1] += 10
+
